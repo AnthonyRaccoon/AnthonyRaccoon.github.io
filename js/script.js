b48b09e5a7bf5c0d64b868fd35347ca13434bb7e
AnthonyPark <65486525+AnthonyRaccoon@users.noreply.github.com>
2023-07-02 19:59:16 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Responsive Website Using HTML And CSS Step By Step - https://youtu.be/IXucQAEkIMo
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+}
+
+const sr = ScrollReveal({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    reset: true
+});
+
+sr.reveal('.hero-text', {delay: 200, origin: 'top'});
+sr.reveal('.hero-img', {delay: 450, origin: 'top'});
+sr.reveal('.icons', {delay: 500, origin: 'left'});
+sr.reveal('.scroll-down', {delay: 450, origin: 'right'});
